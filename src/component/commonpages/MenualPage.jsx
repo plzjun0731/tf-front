fc634885b0fa442c9ed7436bb63095a628d35b67
sooko20 <ksmm0@naver.com>
2025-07-11 15:30:57 +0900

    Add files via upload
    매뉴얼 ui 수정

--- src/component/commonpages/MenualPage.jsx
+++ src/component/commonpages/MenualPage.jsx
@@ -58,14 +58,57 @@ function MenualPage() {
     };
 
     return (
+        // ver1
+        // <div className="menual-page-container">
+        //     <form onSubmit={handleSubmit}>
+        //         <div className="menual-header">
+        //             <h2>업무 매뉴얼</h2>
+        //             <button className="save-button" type="submit">저장</button>
+        //         </div>
+        //         <div className="form-container">
+        //             <div className="left-section">
+        //                 <div className="form-group">
+        //                     <label style={{fontWeight:"bold"}}>DB 상담 멘트</label>
+        //                     <textarea
+        //                         name="script"
+        //                         value={formData.script}
+        //                         onChange={handleChange}
+        //                         placeholder="내용을 입력해주세요."
+        //                     />
+        //                 </div>
+        //             </div>
+        //             <div className="right-section">
+        //                 <div className="form-group">
+        //                     <label style={{fontWeight:"bold"}}>※상담 시 체크사항</label>
+        //                     <textarea
+        //                         name="checklist"
+        //                         value={formData.checklist}
+        //                         onChange={handleChange}
+        //                         placeholder="내용을 입력해주세요."
+        //                     />
+        //                 </div>
+        //                 <div className="form-group">
+        //                     <label style={{fontWeight:"bold"}}>특이사항</label>
+        //                     <textarea
+        //                         name="etc"
+        //                         value={formData.etc}
+        //                         onChange={handleChange}
+        //                         placeholder="내용을 입력해주세요."
+        //                     />
+        //                 </div>
+        //             </div>
+        //         </div>
+        //     </form>
+        // </div>
+
+        // ver2
         <div className="menual-page-container">
             <form onSubmit={handleSubmit}>
                 <div className="menual-header">
                     <h2>업무 매뉴얼</h2>
                     <button className="save-button" type="submit">저장</button>
                 </div>
-                <div className="form-container">
-                    <div className="left-section">
+                    <div className="db-section">
                         <div className="form-group">
                             <label style={{fontWeight:"bold"}}>DB 상담 멘트</label>
                             <textarea
@@ -76,7 +119,7 @@ function MenualPage() {
                             />
                         </div>
                     </div>
-                    <div className="right-section">
+                    <div className="checklist-section">
                         <div className="form-group">
                             <label style={{fontWeight:"bold"}}>※상담 시 체크사항</label>
                             <textarea
@@ -86,6 +129,8 @@ function MenualPage() {
                                 placeholder="내용을 입력해주세요."
                             />
                         </div>
+                    </div>
+                    <div className="etc-section">
                         <div className="form-group">
                             <label style={{fontWeight:"bold"}}>특이사항</label>
                             <textarea
@@ -96,7 +141,6 @@ function MenualPage() {
                             />
                         </div>
                     </div>
-                </div>
             </form>
         </div>
     )
